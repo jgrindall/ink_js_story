a85ffc4787da0776f89775f81893ead302b21c77
jgrindall <john.grindall@gmail.com>
2022-04-10 22:22:50 +0100

    CSS

--- public/src/ParagraphView.vue
+++ public/src/ParagraphView.vue
@@ -1,15 +1,21 @@
 <template>
 
-    <div :class="{'shown': paragraph.shown}">
-        <p>{{paragraph.text}}</p>
-        <p>{{paragraph.tags}}</p>
+    <div class="paragraph"
+         :class="{'shown': paragraph.shown}"
+         :style="{'animation-delay' : paragraph.delay + 's'}"
+    >
+        <p>
+            {{paragraph.text}}
+            <span>{{paragraph.delay}}</span>
+            <span>{{paragraph.tags}}</span>
+        </p>
     </div>
 
 </template>
 
 <script lang="ts" setup>
 
-    import { PropType, defineProps } from 'vue';
+    import { PropType } from 'vue';
     import {Paragraph} from "@/types";
 
     const props = defineProps({
@@ -22,12 +28,27 @@
 </script>
 
 <style lang="scss" scoped>
-    p{
-        background: grey;
-        margin:40px;
-        padding:40px;
+    @keyframes fading {
+        from {
+            opacity: 0;
+        }
+        to {
+            opacity: 1;
+        }
     }
-    .shown{
-        background: lightpink;
+    .paragraph{
+        opacity: 0;
+        &.shown{
+            background: lightpink;
+            animation: fading ease-in 1s;
+            animation-fill-mode: forwards;
+            animation-duration: 1s;
+        }
+        p{
+            background: grey;
+            margin:40px;
+            padding:40px;
+        }
     }
 </style>
+
